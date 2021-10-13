963e92180be04590a504090029cfa2090c2eb587
n.vasileva <n.i.n.a.v@yandex.ru>
2021-10-13 02:27:02 -0700

    feat: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const Path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js',
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html")
+        })
+    ],
+
+}
