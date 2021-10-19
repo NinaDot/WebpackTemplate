28f42faa912b71c8166d6318dc43dae0de45bbf3
n.vasileva <n.i.n.a.v@yandex.ru>
2021-10-19 01:08:14 -0700

    feat: install file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin') 
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -10,7 +11,12 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].js',
+        
+            
     },
+    experiments: {
+        asset: true
+        },
     plugins: [
         new HTMLWebpackPlugin({
             template: path.resolve(__dirname, "src", "index.html")
@@ -18,7 +24,15 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: '[name].css'
         }),
-        new CleanWebpackPlugin ()
+        new CleanWebpackPlugin (),
+        new CopyPlugin({
+            patterns: [
+                {
+                  from: path.resolve(__dirname, 'src/images'),
+                  to:   path.resolve(__dirname, 'dist/images')
+                }
+              ]
+            })
     ],
     module: {
         rules: 
@@ -51,15 +65,15 @@ module.exports = {
                         loader: 'sass-loader'
                     }
                 ]
+            },
+            {
+                test: /\.(jpg|png|svg|jpeg|gif)$/,
+                use: ['file-loader'],
+                type: 'asset/resource'
+            
+           
             }
-        
-        
-        
-       
-
-    ],
-      
-       
+        ]
+        }
+    
     }
-
-}
